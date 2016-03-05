c7c1e8b29ec93aae0fe563a78aeb1b5b58ed6463
canastro <ricardocanastro@gmail.com>
2016-03-05 03:30:37 +0000

    Added README.md

---
+++ README.md
@@ -0,0 +1,57 @@
+# image-contrast
+
+Small library to apply a contrast transformation to a image.
+
+## Install
+
+```
+npm install image-contrast --save
+```
+
+## Usage
+At the moment there are two ways of usage, you either provide a image or you provide a canvas imageData.
+
+### From image:
+
+JS file:
+```js
+var imageContrast = require('image-contrast');
+
+imageContrast({
+    from: '#original',
+    to: '#target-1',
+    contrast: 30
+});
+```
+
+HTML:
+```html
+<img id="original" src="http://lorempixel.com/400/200" />
+<div id="target-1"></div>
+```
+
+### From canvas imageData:
+
+```js
+var canvas = document.createElement('canvas');
+var context = canvas.getContext('2d');
+var img = new Image;
+img.onload = function(){
+    context.drawImage(img,0,0);
+
+    var imageData = context.getImageData(0, 0, img.width, img.height);
+
+    var result3 = imageContrast({
+        imageData: imageData,
+        to: '#target-3',
+        contrast: 30
+    });
+
+    var result4 = imageContrast({
+        imageData: imageData,
+        to: '#target-4',
+        contrast: 70
+    });
+};
+img.src = "http://lorempixel.com/400/200";
+```
--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -15,21 +15,26 @@ window.onload = function () {
 
     var canvas = document.createElement('canvas');
     var context = canvas.getContext('2d');
-    var element = document.getElementById('from_data_original');
-    context.drawImage(element, 0, 0 );
-    var imageData = context.getImageData(0, 0, element.width, element.height);
+    var img = new Image;
+    img.onload = function(){
+        context.drawImage(img,0,0);
 
-    var result3 = imageContrast({
-        imageData: imageData,
-        to: '#target-3',
-        contrast: 30
-    });
-    console.log('result3: ', result3);
+        var imageData = context.getImageData(0, 0, img.width, img.height);
 
-    var result4 = imageContrast({
-        imageData: imageData,
-        to: '#target-4',
-        contrast: 70
-    });
-    console.log(result4);
+        var result3 = imageContrast({
+            imageData: imageData,
+            to: '#target-3',
+            contrast: 30
+        });
+        console.log('result3: ', result3);
+
+        var result4 = imageContrast({
+            imageData: imageData,
+            to: '#target-4',
+            contrast: 70
+        });
+        console.log(result4);
+    };
+    img.src = "http://lorempixel.com/400/200";
+
 }
